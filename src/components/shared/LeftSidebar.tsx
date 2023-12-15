3e14177dc0fcfeb5988020ac0cdd74688a7b1dd6
unknown <joseph.harley02@gmail.com>
2023-12-14 20:17:54 -0500

    created highlight on active link and fixed issue where icons weren't showing when hovered

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,4 +1,4 @@
-import { Link, NavLink, useNavigate } from "react-router-dom"
+import { Link, NavLink, useNavigate, useLocation } from "react-router-dom"
 import { Button } from "../ui/button"
 import { useSignOutAccount } from "@/lib/react-query/queriesandMutations"
 import { useEffect } from "react";
@@ -7,15 +7,15 @@ import { INavLink } from "@/types";
 import { sidebarLinks } from "@/constants";
 
 const LeftSidebar = () => {
-    const { mutate: signOut, isSuccess } = useSignOutAccount();
-    const navigate = useNavigate();
-    const { user } = useUserContext();
-
-    useEffect(() => {
-        if (isSuccess) {
-            navigate(0);
-        }
-    }, [isSuccess]);
+  const { pathname } = useLocation();
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  const { user } = useUserContext();
+  useEffect(() => {
+      if (isSuccess) {
+          navigate(0);
+      }
+  }, [isSuccess]);
   return (
     <nav className="leftsidebar">
       <div className="flex flex-col gap-11">
@@ -46,13 +46,15 @@ const LeftSidebar = () => {
 
         <ul className="flex flex-col gap-6">
           {sidebarLinks.map((link: INavLink) => {
+            const isActive= pathname === link.route;
+
             return (
-              <li key={link.label} className="leftsidebar-link">
+              <li key={link.label} className={`leftsidebar-link group ${isActive && "bg-primary-500"}`}>
                 <NavLink to={link.route} className="flex gap-4 items-center p-4">
                   <img 
                     src={link.imgURL}
                     alt={link.label}
-                    className="group-hover:invert-white"
+                    className={`group-hover:invert-white ${isActive && "invert-white"}`}
                   />
                   {link.label}
                 </NavLink>
